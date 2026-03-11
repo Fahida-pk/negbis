836f710c005461994bb0744d0834f8bf62b61d01
Fahida-pk <118457841+Fahida-pk@users.noreply.github.com>
2026-03-11 12:29:18 +0530

    update project

--- src/Pages/Dashboard/dashboard.jsx
+++ src/Pages/Dashboard/dashboard.jsx
@@ -18,7 +18,7 @@ function Dashboard() {
     >
 
       {/* TOP MENU */}
-      <div style={{ display: "flex", gap: "10px", padding: "10px", background: "blue" }}>
+      <div style={{ display: "flex", gap: "10px", padding: "10px", background: "#1c2a3a" }}>
         <span style={{ cursor: "pointer" }}>Dashboard</span>
 
         <span
@@ -34,42 +34,44 @@ function Dashboard() {
       <div
         style={{
           position: "absolute",
-          bottom: "80px",
+          bottom: "140px",
           width: "100%",
           display: "flex",
           justifyContent: "space-between",
           alignItems: "center",
-          padding: "0 120px"
+          padding: "0 160px"
         }}
       >
 
         {/* LEFT SIDE */}
-        <div style={{ display: "flex", alignItems: "center", gap: "20px" }}>
+        <div style={{ display: "flex", alignItems: "center", gap: "25px" }}>
           <img
-  src="/image/logo.webp"
-  style={{
-    width: "80px",
-    filter: "brightness(0) invert(1)"
-  }}
-/>
+            src="/image/logo.webp"
+            style={{
+              width: "140px",
+              filter: "brightness(0) invert(1)"
+            }}
+          />
 
           <div>
-            <h2 style={{ margin: 0 }}>CodeZyntax</h2>
-            <p style={{ margin: 0 }}>Softwares L.L.P</p>
+            <h2 style={{ margin: 0, fontSize: "28px" }}>CodeZyntax</h2>
+            <p style={{ margin: 0, fontSize: "20px" }}>Softwares L.L.P</p>
           </div>
         </div>
 
 
         {/* CENTER */}
         <div style={{ textAlign: "center" }}>
-          <p style={{ margin: 0 }}>+91 88484 18551</p>
-          <p style={{ margin: 0 }}>contact@codezyntax.com</p>
+          <p style={{ margin: 0, fontSize: "20px" }}>+91 88484 18551</p>
+          <p style={{ margin: 0, fontSize: "20px" }}>contact@codezyntax.com</p>
         </div>
 
 
         {/* RIGHT */}
         <div>
-          <h1 style={{ margin: 0 }}>neGbis ERP v8.0.6</h1>
+          <h1 style={{ margin: 0, fontSize: "36px" }}>
+            neGbis ERP v8.0.6
+          </h1>
         </div>
 
       </div>
