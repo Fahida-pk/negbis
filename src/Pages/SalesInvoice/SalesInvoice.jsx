30628a12738c67236c2700476c71c218863b2bae
Fahida-pk <118457841+Fahida-pk@users.noreply.github.com>
2026-03-11 13:12:17 +0530

    update project

--- src/Pages/SalesInvoice/SalesInvoice.jsx
+++ src/Pages/SalesInvoice/SalesInvoice.jsx
@@ -1,68 +1,76 @@
 import { useState } from "react";
 
-function SalesInvoice(){
+function SalesReport(){
 
-const [fromDate,setFromDate]=useState("");
-const [toDate,setToDate]=useState("");
-const [data,setData]=useState([]);
+const [from,setFrom] = useState("");
+const [to,setTo] = useState("");
+const [data,setData] = useState([]);
 
-const loadData=async()=>{
+const loadReport = async () =>{
 
-const res=await fetch(
-`https://negbis.codezyntax.com/api/salesSummary.php?from=${fromDate}&to=${toDate}`
+const res = await fetch(
+`http://localhost/api/sales_summary.php?from=${from}&to=${to}`
 );
 
-const result=await res.json();
+const result = await res.json();
 
 setData(result);
 
-};
+}
 
 return(
 
 <div style={{padding:"20px"}}>
 
-<h2>Sales Invoice Report</h2>
+<h2>Sales Summary Report</h2>
 
+From :
 <input
 type="date"
-value={fromDate}
-onChange={(e)=>setFromDate(e.target.value)}
+value={from}
+onChange={(e)=>setFrom(e.target.value)}
 />
 
+To :
 <input
 type="date"
-value={toDate}
-onChange={(e)=>setToDate(e.target.value)}
+value={to}
+onChange={(e)=>setTo(e.target.value)}
 />
 
-<button onClick={loadData}>Search</button>
+<button onClick={loadReport}>
+Print
+</button>
 
-<table border="1" width="100%" style={{marginTop:"20px"}}>
+<table border="1" width="100%">
 
-<thead style={{background:"#1c2a3a",color:"white"}}>
+<thead>
 
 <tr>
-<th>Invoice No</th>
+
+<th>Sale No</th>
 <th>Date</th>
 <th>Customer</th>
 <th>Net Amount</th>
 <th>Gross Amount</th>
+<th>Status</th>
+
 </tr>
 
 </thead>
 
 <tbody>
 
-{data.map((item,index)=>(
+{data.map((item)=>(
 
-<tr key={index}>
+<tr key={item.id}>
 
-<td>{item.SALE_NO}</td>
-<td>{item.SALE_DATE}</td>
-<td>{item.CUST_NAME}</td>
-<td>{item.NET_AMOUNT}</td>
-<td>{item.GROSS_AMOUNT}</td>
+<td>{item.sale_no}</td>
+<td>{item.sale_date}</td>
+<td>{item.cust_name}</td>
+<td>{item.net_amount}</td>
+<td>{item.gross_amount}</td>
+<td>{item.status_desc}</td>
 
 </tr>
 
@@ -74,8 +82,8 @@ onChange={(e)=>setToDate(e.target.value)}
 
 </div>
 
-);
+)
 
 }
 
-export default SalesInvoice;
+export default SalesReport;
