543a85f5e6ba4c3e7088c1702a0d3c394ad69fe6
Fahida-pk <118457841+Fahida-pk@users.noreply.github.com>
2026-03-11 12:07:19 +0530

    update dashboard and add background image

--- src/Pages/Dashboard/dashboard.jsx
+++ src/Pages/Dashboard/dashboard.jsx
@@ -5,12 +5,25 @@ function Dashboard() {
   const navigate = useNavigate();
 
   return (
-    <div style={{ background: "#0f2f5a", minHeight: "100vh", color: "white" }}>
+
+    <div
+      style={{
+        backgroundImage: "url('/image/bagroundimage.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        minHeight: "100vh",
+        color: "white"
+      }}
+    >
 
       {/* TOP MENU */}
       <div style={{ display: "flex", gap: "20px", padding: "15px", background: "#1c2a3a" }}>
         <span style={{ cursor: "pointer" }}>Dashboard</span>
-        <span style={{ cursor: "pointer" }} onClick={() => navigate("/reports")}>
+
+        <span
+          style={{ cursor: "pointer" }}
+          onClick={() => navigate("/reports")}
+        >
           Reports
         </span>
       </div>
@@ -20,15 +33,21 @@ function Dashboard() {
 
         <h1>Dashboard</h1>
 
-        <div style={{ marginTop: "200px" }}>
+        {/* BOTTOM TEXT */}
+        <div style={{ position: "absolute", bottom: "120px", left: "60px" }}>
+
           <h2>CodeZyntax Softwares L.L.P</h2>
+
           <p>+91 88484 18551</p>
+
           <h2>neGbis ERP v8.0.6</h2>
+
         </div>
 
       </div>
 
     </div>
+
   );
 }
 
