7a484b33dc76e892e6aead4c1fe89c7d6cfb5786
Fahida-pk <118457841+Fahida-pk@users.noreply.github.com>
2026-03-14 14:33:17 +0530

    update project

--- api/data.js
+++ api/data.js
@@ -30,60 +30,6 @@ export default async function handler(req, res) {
 
     }
 
-    else if (type === "dailySalesReport") {
-
-      url = `https://erp.codezyntax.com/api/dailySalesReport.php?from=${from}&to=${to}&store=${store}`;
-
-    }
-
-    else if (type === "dailySalesSummary") {
-
-      url = `https://erp.codezyntax.com/api/dailySalesSummary.php?from=${from}&to=${to}&store=${store}`;
-
-    }
-
-    else if (type === "itemwiseSales") {
-
-      url = `https://erp.codezyntax.com/api/itemwiseSales.php?from=${from}&to=${to}&store=${store}`;
-
-    }
-
-    else if (type === "itemwiseProfit") {
-
-      url = `https://erp.codezyntax.com/api/itemwiseProfit.php?from=${from}&to=${to}&store=${store}`;
-
-    }
-
-    else if (type === "itemwiseSummary") {
-
-      url = `https://erp.codezyntax.com/api/itemwiseSummary.php?from=${from}&to=${to}&store=${store}`;
-
-    }
-
-    else if (type === "salesProfit") {
-
-      url = `https://erp.codezyntax.com/api/salesProfit.php?from=${from}&to=${to}&store=${store}`;
-
-    }
-
-    else if (type === "salesmanSales") {
-
-      url = `https://erp.codezyntax.com/api/salesmanSales.php?from=${from}&to=${to}&store=${store}`;
-
-    }
-
-    else if (type === "salesTaxSummary") {
-
-      url = `https://erp.codezyntax.com/api/salesTaxSummary.php?from=${from}&to=${to}&store=${store}`;
-
-    }
-
-    else if (type === "saleDetails") {
-
-      url = `https://erp.codezyntax.com/api/saleDetails.php?from=${from}&to=${to}&store=${store}`;
-
-    }
-
     else if (type === "customerLookup") {
 
       url = `https://erp.codezyntax.com/api/customerLookup.php`;
