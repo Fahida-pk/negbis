965002582052970884f7e77cb80b0d9058e0bc37
Fahida-pk <118457841+Fahida-pk@users.noreply.github.com>
2026-03-18 16:56:47 +0530

    update project

--- src/Pages/Reports/SalesReports.jsx
+++ src/Pages/Reports/SalesReports.jsx
@@ -98,9 +98,19 @@ url =
 &billWise=${billWise ? 1 : 0}`
 
 }
-else if (type === "monthlySalesSummary") {
+else if(report === "monthly_summary"){
 
-  url = `https://erp.codezyntax.com/api/monthly_Sales_Summary.php?from=${from}&to=${to}&store=${store || 0}&custid=${custid || 0}&opts=${opts || 0}&stype=${stype || 0}&status=${status || ""}&salesman=${salesman || 0}&user=${user || 0}&estimate=${estimate || 0}`;
+url =
+`/api/data?type=monthlySalesSummary
+&from=${fromDate}
+&to=${toDate}
+&store=${Number(store)||0}
+&custid=${Number(customerCode)||0}
+&opts=${opts}
+&stype=${stype}
+&status=${status.join(",")}
+&user=${user}
+&salesman=${salesman}`
 
 }
 console.log("API URL:",url)
